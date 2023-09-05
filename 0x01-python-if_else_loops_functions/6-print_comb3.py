3e06310f23f2d27beb7c126488100ab712acab39
khalilharir <khalil.harir2@gmail.com>
2023-09-05 14:30:47 -0700

    comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(i + 1, 10):  
-        if i == 8 and j == 9:
-            print(f"{i}{j}") 
-        else:
-            print(f"{i}{j}, ", end="") 
+    for j in range(i + 1, 10):
+        print(f"{i:02d}, {j:02d}")
