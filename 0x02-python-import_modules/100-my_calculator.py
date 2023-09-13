a2a636d144272b0ef3a8ef7b39d1480c71e43b35
khalilharir <khalil.harir2@gmail.com>
2023-09-13 06:59:24 -0700

    cal

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
-    import sys
-
-    if len(sys.argv) - 1 != 3:
+    from sys import argv, exit
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+        exit(1)
+    if argv[2] not in {"+", "-", "/", "*"}:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
-
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+    if op == '+':
+        result = add(a, b)
+    if op == '-':
+        result = sub(a, b)
+    if op == '*':
+        result = mul(a, b)
+    if op == '/':
+        result = div(a, b)
+    print(f"{a} {op} {b} = {result}")
