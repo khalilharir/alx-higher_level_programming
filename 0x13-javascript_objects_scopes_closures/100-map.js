5a89a2929cf870c5497d34fca0767e710ac5283c
khalilharir <khalil.harir2@gmail.com>
2024-01-09 00:18:03 -0800

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const L1 = require('./100-data').list;
-const map_1 = list1.map((num, index) => num * i);
-console.log(L1);
-console.log(map_1);
+const list1 = require('./100-data').list;
+const map1 = list1.map((num, index) => num * index);
+console.log(list1);
+console.log(map1);
